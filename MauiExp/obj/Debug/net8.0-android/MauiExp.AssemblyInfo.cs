4c9fd9f65bd4e9cc4d57212c947befb85230e7bb
bolfosu <bolfamihail@gmail.com>
2024-04-27 00:48:47 +0200

    added Android dial api

--- MauiExp/obj/Debug/net8.0-android/MauiExp.AssemblyInfo.cs
+++ MauiExp/obj/Debug/net8.0-android/MauiExp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiExp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1dc4c84771f8681288a25ef8bb5cec75d870126c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+420301687b94f45500dd3caa6ba78167a45c2d13")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiExp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiExp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
